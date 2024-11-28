487c5003bdb25643e8baeef39ae4a5be91494b3a
mahmed960 <ahmedmirzaa8@gmail.com>
2024-11-28 15:07:15 +0500

    Test empty  Commit

--- app/page.js
+++ app/page.js
@@ -8,7 +8,7 @@ export default function Home() {
       <Slider/>
       <div className="my-10 md:my-20 flex justify-center items-center">
           <div className="w-full sm:w-[75%] text-center">
-            <p><strong>Akbar Aslam</strong> is a luxury womens ethnic brand that combines modern aesthetic with age-old traditions. Our handcrafted and hand-embellished dresses are made by master craftsmen, ensuring the highest quality and commitment to our customers.</p>
+            <p><strong>Akbar Aslams</strong> is a luxury womens ethnic brand that combines modern aesthetic with age-old traditions. Our handcrafted and hand-embellished dresses are made by master craftsmen, ensuring the highest quality and commitment to our customers.</p>
             <h3 className="font-semibold my-5">International Customers Updates:</h3>
             <p>No Tax or Import Duties. Parcels will be shipped from local UK center with new reduced shipping rate.</p>
           </div>
